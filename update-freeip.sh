7025d8fcad5f5adfc4961a613054e31b22781d10
stubbornrock <rockstubborn@hotmail.com>
2025-10-05 17:45:50 +0800

    Update update-freeip.sh

--- update-freeip.sh
+++ update-freeip.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-V2RAY_URL="https://bgithub.xyz/Alvin9999/new-pac/wiki/v2ray%E5%85%8D%E8%B4%B9%E8%B4%A6%E5%8F%B7"
-SSR_URL="https://bgithub.xyz/Alvin9999/new-pac/wiki/ss%E5%85%8D%E8%B4%B9%E8%B4%A6%E5%8F%B7"
+#V2RAY_URL="https://bgithub.xyz/Alvin9999/new-pac/wiki/v2ray%E5%85%8D%E8%B4%B9%E8%B4%A6%E5%8F%B7"
+#SSR_URL="https://bgithub.xyz/Alvin9999/new-pac/wiki/ss%E5%85%8D%E8%B4%B9%E8%B4%A6%E5%8F%B7"
 
-#V2RAY_URL="https://github.com/Alvin9999/new-pac/wiki/v2ray%E5%85%8D%E8%B4%B9%E8%B4%A6%E5%8F%B7"
-#SSR_URL="https://github.com/Alvin9999/new-pac/wiki/ss%E5%85%8D%E8%B4%B9%E8%B4%A6%E5%8F%B7"
+V2RAY_URL="https://github.com/Alvin9999/new-pac/wiki/v2ray%E5%85%8D%E8%B4%B9%E8%B4%A6%E5%8F%B7"
+SSR_URL="https://github.com/Alvin9999/new-pac/wiki/ss%E5%85%8D%E8%B4%B9%E8%B4%A6%E5%8F%B7"
 
 NOW=$(date +"%Y-%m-%d %H:%M:%S")
 echo "#Date: $NOW" > links.txt
